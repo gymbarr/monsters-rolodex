162d0a756b2b2d7f046d78bc586b91e4907d9afe
Andrey Timakhovich <stewart96.at@gmail.com>
2022-12-03 20:57:39 +0300

    add search input for monsters name and filter monsters by it

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchInput: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {
-        console.log(this.state);
-      }
-      ));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            // console.log(this.state);
+          }
+        )
+      );
+  }
+
+  setSearchInput(inputValue) {
+    this.setState({
+      searchInput: inputValue,
+    });
+  }
+
+  isNameMatch(monster) {
+    const searchValue = this.state.searchInput;
+    return monster.name.match(
+      new RegExp(`(^${searchValue}| ${searchValue})`, "i")
+    );
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return (
-              <div>
-                <h1>{monster.name}</h1>
-              </div>
-            );
-          })
-        }
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event) => this.setSearchInput(event.target.value)}
+        />
+
+        {this.state.monsters
+          .filter((monster) => this.isNameMatch(monster))
+          .map((monster) => (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          ))}
       </div>
     );
   }
